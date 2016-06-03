27ee3085bfc45d5743ea0666d7a5bf7618c4b06a
BaezCrdrm <dev.baez_crdrm@outlook.com>
2016-06-03 02:12:43 -0500

    Pequeños cambios 1

---
+++ scripts/vista.js
@@ -0,0 +1,14 @@
+function cambioImg()
+{
+    //http://www.w3schools.com/jsref/event_onscroll.asp
+    
+    if(document.body.scrollTop > 300)
+    {
+        
+    }
+}
+
+function hola(elemento)
+{
+    elemento.style.backgroundImage = "url(../img/wip1.jpg)";
+}
