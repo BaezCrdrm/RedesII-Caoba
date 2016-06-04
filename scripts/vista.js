f4c82b48ec44d52c44917929d16c501587c04bd9
Samuel Báez <dev.baez_crdrm@outlook.com>
2016-06-03 19:16:21 -0500

    Revision (#4)
    * Pequeños cambios 1
    
    * Listo?

---
+++ scripts/vista.js
@@ -0,0 +1,19 @@
+function cambioImg()
+{
+    //http://www.w3schools.com/jsref/event_onscroll.asp
+    
+    if(document.body.scrollTop > 300)
+    {
+        
+    }
+}
+
+function hola(elemento)
+{
+    elemento.style.backgroundImage = "url(../img/wip1.jpg)";
+}
+
+function desplaza(cantidad)
+{
+    window.scrollTo(0, cantidad);
+}
